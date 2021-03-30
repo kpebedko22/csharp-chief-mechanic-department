f2514754a569117095e678b71d77be792eb97292
Alexander <36397871+kpebedko22@users.noreply.github.com>
2021-03-30 14:55:38 +0700

    Открытие договора лизинга

--- LeasingViewForm.cs
+++ LeasingViewForm.cs
@@ -7,13 +7,41 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Word = Microsoft.Office.Interop.Word;
 
 namespace OGM {
 	public partial class LeasingViewForm : Form {
 		public LeasingViewForm() {
 			InitializeComponent();
 
+			OpenLeasingContract();
+		}
 
+		private void OpenLeasingContract() {
+
+			try {
+				Word.Application app = new Word.Application();
+				Object fileName = @"C:\Users\User\Documents\GitHub\OGM_v1\dogovor-lizinga-oborudovaniya.doc";
+				Object missing = Type.Missing;
+				app.Documents.Open(ref fileName);
+
+				Word.Document doc = app.ActiveDocument;
+
+				for (int i = 1; i < doc.Paragraphs.Count; i++) {
+					string parText = doc.Paragraphs[i].Range.Text;
+					richTextBox_LeasingContract.Text += parText;
+				}
+
+				app.ActiveDocument.Close();
+				app.Quit();
+			}
+			catch (Exception e) {
+				// сообщение об ошибке
+				MessageBox.Show(e.Message);
+			}
+			
+
+			
 		}
 	}
 }
