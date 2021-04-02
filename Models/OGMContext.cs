d7dbce36db1f802f439648ef21f3e3d1e63fe87a
ht-evth <ht-evth@users.noreply.github.com>
2021-04-02 14:11:18 +0700

    тест бд

---
+++ Models/OGMContext.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+
+using System.Windows.Forms;
+
+namespace OGM.Models
+{
+    public partial class OGMContext : DbContext
+    {
+
+        public OGMContext()
+        {
+
+
+            try
+            {
+                Database.EnsureCreated();
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message);
+            }
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseMySql(
+                "server=25.36.221.15;user=application1;password=Qwerty_1;database=ogm_db;"
+            );
+        }
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -4,13 +4,19 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+using OGM.Models;
+
 namespace OGM {
 	static class Program {
+
+		public static OGMContext db;
+
 		/// <summary>
 		/// Главная точка входа для приложения.
 		/// </summary>
 		[STAThread]
 		static void Main() {
+			db = new OGMContext();
 			Application.EnableVisualStyles();
 			Application.SetCompatibleTextRenderingDefault(false);
 			Application.Run(new LogInForm());
